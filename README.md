1453b6779933b8f929d91087f1b85621c7029f3e
Yusuf Unal <theunal9@gmail.com>
2023-05-26 23:16:23 +0300

    v1.0.9

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ import { fileToBase64 } from 'ngx-base64';
 let file = event.target.files[0];
 
 fileToBase64(file).subscribe(res => {
-  console.log(res)
-})
+  console.log(res);
+});
+
+let base64String = fileToBase64Sync(file);
+console.log(base64String);
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import { Observable, Subscriber } from 'rxjs';
 
-const fileToBase64 = (file: any): Observable<any> => {
-    return new Observable((subscriber: Subscriber<any>) => {
-        const filereader = new FileReader();
-        filereader.readAsDataURL(file);
+const reader = (file: any, subscriber: Subscriber<any>) => {
+    const filereader = new FileReader();
+    filereader.readAsDataURL(file);
 
-        filereader.onload = () => {
-            subscriber.next(filereader.result);
-            subscriber.complete();
-        };
-        filereader.onerror = (error) => {
-            subscriber.error(error);
-            subscriber.complete();
-        };
-    })
-}
+    filereader.onload = () => {
+        subscriber.next(filereader.result);
+        subscriber.complete();
+    };
+    filereader.onerror = (error: any) => {
+        subscriber.error(error);
+        subscriber.complete();
+    };
+};
+
+const fileToBase64 = (file: any): Promise<string> => new Observable((subscriber: Subscriber<any>) => {
+    reader(file, subscriber);
+}).toPromise();
 
 export { fileToBase64 };
